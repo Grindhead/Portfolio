4c748ef18bb346bf5f8e2cb0e1e9ac67b88a07f0
Craig Beswetherick <craigbeswetherick@gmail.com>
2023-06-19 00:20:17 +0100

    added basic packages

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
