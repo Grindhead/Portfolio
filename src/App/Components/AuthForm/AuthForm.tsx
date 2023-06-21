ba38ef583e2726094c435f21fff148d61ba49a31
Craig Beswetherick <craigbeswetherick@gmail.com>
2023-06-21 02:01:47 +0100

    moved the auth form into its own component for reuse

---
+++ src/App/Components/AuthForm/AuthForm.tsx
@@ -0,0 +1,66 @@
+import {
+  Box,
+  TextField,
+  InputAdornment,
+  IconButton,
+  Button,
+} from "@mui/material";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+interface AuthFormProps {
+  handleSignUp: (email: string, password: string) => void;
+}
+
+export const AuthForm = ({ handleSignUp }: AuthFormProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box component="form">
+      <TextField
+        id="email"
+        label="Email Address"
+        variant="standard"
+        fullWidth
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <TextField
+        fullWidth
+        id="password"
+        label="Password"
+        variant="standard"
+        type={showPassword ? "text" : "password"}
+        autoComplete="current-password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button onClick={() => handleSignUp(email, password)}>Sign Up</Button>
+    </Box>
+  );
+};
--- src/App/Pages/SignUp/SignUp.tsx
+++ src/App/Pages/SignUp/SignUp.tsx
@@ -1,32 +1,15 @@
 import { useState } from "react";
-import {
-  Box,
-  Typography,
-  TextField,
-  InputAdornment,
-  IconButton,
-  Button,
-} from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Typography } from "@mui/material";
 import { emailAndPassword } from "../../Utils/Firebase";
 import { useNavigate } from "react-router-dom";
+import { AuthForm } from "../../Components/AuthForm/AuthForm";
 
 const SignUp = () => {
   const [error, setError] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+
   const navigate = useNavigate();
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
-
-  const handleSignUp = () => {
+  const handleSignUp = (email: string, password: string) => {
     emailAndPassword(email, password).then((userCredential) => {
       if (userCredential instanceof Error) {
         setError(userCredential.message);
@@ -39,48 +22,13 @@ const SignUp = () => {
   return (
     <div>
       <Typography variant="h2">SignUp</Typography>
-      <Box component="form">
-        <TextField
-          id="email"
-          label="Email Address"
-          variant="standard"
-          fullWidth
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <TextField
-          fullWidth
-          id="password"
-          label="Password"
-          variant="standard"
-          type={showPassword ? "text" : "password"}
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Button onClick={() => handleSignUp()}>Sign Up</Button>
-        <br />
-        {error && (
-          <Typography variant="body1" color="error">
-            {error}
-          </Typography>
-        )}
-      </Box>
+      <AuthForm handleSignUp={handleSignUp} />
+      <br />
+      {error && (
+        <Typography variant="body1" color="error">
+          {error}
+        </Typography>
+      )}
     </div>
   );
 };
