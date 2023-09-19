a2f846fd5e041f6d363c2d8d68fcaca9976d80c2
Craig Beswetherick <craigbeswetherick@gmail.com>
2023-09-19 01:24:41 +0100

    tags

--- scripts/createPosts.mjs
+++ scripts/createPosts.mjs
@@ -1,9 +1,9 @@
-import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
-import { deletePosts} from './deletePosts.mjs';
-import config from '../../id.json' assert { type: 'json' };
+import admin from "firebase-admin";
+import { readFileSync } from "fs";
+import { deletePosts } from "./deletePosts.mjs";
+import config from "../../id.json" assert { type: "json" };
 
-const serviceAccount = JSON.parse(readFileSync("../key.json", 'utf-8'));
+const serviceAccount = JSON.parse(readFileSync("../key.json", "utf-8"));
 
 export const app = await admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,20 +14,20 @@ export const db = admin.firestore();
 
 export const addPosts = async () => {
   try {
-
     await deletePosts();
 
     const adminId = config.id;
     const postsRef = db.collection("posts");
 
     for (var i = 0; i < 30; i++) {
-      const postCount = await postsRef.count().get()
+      const postCount = await postsRef.count().get();
       const newPostRef = await postsRef.add({
         title: "title " + i.toString(),
         description: "description " + i.toString(),
         content: "content " + i.toString(),
         authorId: adminId,
-        id: postCount.data().count
+        id: postCount.data().count,
+        tags: ["tag1", "tag2", "tag3"],
       });
     }
     console.log("Created Documents");
