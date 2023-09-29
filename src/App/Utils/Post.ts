2bdfd85517e69235bba655e4e775c101ca3a4b78
Craig Beswetherick <craigbeswetherick@gmail.com>
2023-09-29 01:26:18 +0100

    bug fixes

--- src/App/Utils/Post.ts
+++ src/App/Utils/Post.ts
@@ -1,11 +1,9 @@
-const post = {
-  title: "",
-  description: "",
-  content: "",
-  id: "",
-  tags: "",
-  tagList: "",
-  authorId: "",
-} as const;
-
-export type PostType = [keyof typeof post];
+export type PostType = {
+  title: "";
+  description: "";
+  content: "";
+  id: "";
+  tags: "";
+  tagList: string[];
+  authorId: "";
+};
